16e504a9c53dd2601af27cd1ccfeab533439c8d6
nobilelucifero <nobile.lucifero@gmail.com>
2013-11-24 22:03:46 +0100

    rewrite Lazyfill * now is just a one-shot JS file. * no need to initialize it by hand * shorter code * now compatible with IE9 * less debug infos * includes `resize` event, but does nothing is the `src` is the same

--- lazyfill.js
+++ lazyfill.js
@@ -1,111 +1,80 @@
-function Lazyfill() {
+(function () {
 
-    this.mediaControl = $('.js-media');
+    'use strict';
 
-    this.getPseudoProperty = function (el, context, property) {
-        return window.getComputedStyle(el, context).getPropertyValue(property).replace(/\"|\'/g, '');
-    };
+    var lazyfill = function () {
 
-    this._getMediaCurrent = function (el) {
-        var media = this.getPseudoProperty(el, ':before', 'content');
-        var isRetina = this.getPseudoProperty(el, ':after', 'content');
-
-        // If no media is provided, fallback on default
-        if (media === '' || !media) {
-            media = 'default';
+        function getPseudoProperty (el, context, property) {
+            return window.getComputedStyle(el, context).getPropertyValue(property).replace(/\"|\'/g, '');
         }
 
-        return media;
-    };
+        var mediaControl = document.querySelector('.js-media');
 
-    this._source2src = function (els, media) {
+        if (!mediaControl) {
+            var body = document.querySelector('body');
+
+            mediaControl = document.createElement('div');
+            mediaControl.className = 'js-media';
+            body.appendChild(mediaControl);
+
+            // Debug
+            console.info('[Lazyfill] INIT');
+        }
+
+        var els = document.querySelectorAll('.js-lazyfill');
+        var media = getPseudoProperty(mediaControl, ':before', 'content');
+
+        // If no media is provided, fallback on default
+        if (media === '' || !media) { media = 'default'; }
+
+        // Detect changes, false by default
+        var changed = false;
+
         // Loop through all the found images
-        var loop = function () {
-            // Detect changes, false by default
-            var changed = false;
-            // Loop through all the found images
-            for (var i = 0; i < els.length; i++) {
-                // simplify the syntax
-                var el       = els[i],
-                    // jsonify data-imageset
-                    sources  = JSON.parse(el.getAttribute('data-sources'));
-                    // extract the value from imageSet
-                    src      = sources[media];
-                    // Old src
-                    srcOld   = el.getAttribute('src');
+        for (var i = 0; i < els.length; i++) {
 
-                if (!sources[media]) { src = sources['default']; }
+            var el       = els[i],
+                sources  = JSON.parse(el.getAttribute('data-sources')),
+                // extract the value from imageSet
+                src      = sources[media],
+                // Old src
+                srcOld   = el.getAttribute('src');
 
-                if (srcOld == src) {
-                    continue;
-                }
+            if (!sources[media]) { src = sources['default']; }
 
-                changed = true;
-
-                // Provide also an [alt] attribute
-                var alt = el.getAttribute('data-alt');
-
-                if (!alt) { alt = ''; }
-
-                // Set the [src] attribute
-                el.setAttribute('src', src);
-                // Set alse the [alt] attribute, this avoid some flickering while setting [src]
-                el.setAttribute('alt', alt);
+            if (srcOld == src) {
+                continue;
             }
 
-            return changed;
-        };
+            // Provide also an [alt] attribute
+            var alt = el.getAttribute('data-alt');
+
+            if (!alt) { alt = ''; }
+
+            // Set the [src] attribute
+            el.setAttribute('src', src);
+            // Set alse the [alt] attribute, this avoid some flickering while setting [src]
+            el.setAttribute('alt', alt);
+
+            changed = true;
+        }
 
         // Debug
-        if (loop()) {
+        if (changed && console.groupCollapsed) {
             console.groupCollapsed('[Lazyfill]', 'Elements found:', els.length, 'Media is:', media);
             console.table(els, ['src', 'alt', 'className']);
             console.groupEnd();
         }
+
     };
 
-    this._calculate = function () {
-        // Get the images we want, assign the found media support
-        this._source2src($$('.js-lazyfill'), this._getMediaCurrent(this.mediaControl));
-    };
+    if (window.addEventListener) {
+        window.addEventListener("DOMContentLoaded", function () {
+            lazyfill();
+            // detach the initial event
+            window.removeEventListener("DOMContentLoaded", lazyfill, false);
+        }, false );
+        window.addEventListener("resize", lazyfill, false);
+    }
 
-    this.init = function () {
-        // Debug
-        console.info('[Lazyfill] INIT');
-
-        if (!this.mediaControl) {
-            // Define body and mediaControl
-            var body = document.querySelector('body'),
-                mediaControl = document.createElement('div');
-
-            // Assign a class
-            mediaControl.classList.add('js-media');
-            // Append to the body
-            body.appendChild(mediaControl);
-
-            // Debug
-            console.log('[Lazyfill] Place media control');
-
-            this.mediaControl = mediaControl;
-        }
-
-        // Wraps the arguments
-        this._calculate();
-
-        // Handle the rezise event
-        this._initOnResize();
-    };
-
-    this._initOnResize = function () {
-
-        // Preserve the old context
-        var that = this;
-
-        window.addEventListener(
-            'resize',
-            _.debounce(that._calculate.bind(that), 1000),
-            false
-        );
-    };
-
-}
+}(this));
